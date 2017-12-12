a4fcffb56296d7656e3fb323e4d997706eec1ad8
alexauff <alexandra.auffray@gmail.com>
2017-12-12 11:56:00 +0100

    creation page trips, correction pb footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'home/map' 
+  get 'home/trips'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
