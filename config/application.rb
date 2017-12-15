f2531fce7a7053c81ce94919d295ad754b7e6b01
Halvard75 <metivier.anto@gmail.com>
2017-12-15 12:19:35 +0100

    push sur heroku avec prise en compte Js 11

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,8 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require *Rails.groups(:assets)
+Bundler.require(*Rails.groups)
+
 
 module IMakeMyTrip
   class Application < Rails::Application
