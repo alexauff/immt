2c382a6e322711487f7907c958c8e19e1a6d3926
Halvard75 <metivier.anto@gmail.com>
2017-12-15 14:52:55 +0100

    push sur heroku avec prise en compte Js 12

---
+++ public/assets/add-marker-2da58eba848e457419df4131171caec5eea2a7ec99edb628776f109e73ab71c3.js
@@ -0,0 +1,29 @@
+// https://developers.google.com/maps/documentation/javascript/examples/geocoding-place-id
+// Ajouter un marker sur la carte en fonction de l'ID d'une place :
+//
+//var geocoder = new google.maps.Geocoder;
+//
+//function geocodePlaceId(geocoder, map, infowindow) {
+//    var placeId = document.getElementById('place-id').value;
+//    geocoder.geocode({
+//        'placeId': placeId
+//    }, function (results, status) {
+//        if (status === 'OK') {
+//            if (results[0]) {
+//                map.setZoom(11);
+//                map.setCenter(results[0].geometry.location);
+//                var marker = new google.maps.Marker({
+//                    map: map,
+//                    position: results[0].geometry.location
+//                });
+//            } else {
+//                alert('No results found');
+//            }
+//        } else {
+//            alert('Geocoder failed due to: ' + status);
+//        }
+//    });
+//
+//}
+//
+//});
