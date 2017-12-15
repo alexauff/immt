6cf8a2645782e452a60fabc279aa2cf467dfad92
alexauff <alexandra.auffray@gmail.com>
2017-12-15 14:40:45 +0100

    modif page connexion avec isabelle alex

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	before_action :sanitize_devise_params, if: :devise_controller?
-
+	layout :layout_by_resource
 	def sanitize_devise_params
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 	end
+
+	def layout_by_resource
+    if devise_controller?
+      'deviseLayout'
+    end
+  end
+
 	
 	end
